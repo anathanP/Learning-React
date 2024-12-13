dab40f55a35c17833b5c01c29844ed90461d5385
anathanP <abbasmasoudardakani@gmail.com>
2024-12-13 19:02:11 +0330

    modifyingalert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [display, setDisplay] = useState(false);
+  const handleOnClick = (state: boolean) => {
+    setDisplay(state);
+  };
   return (
     <div>
-      <Button color="success" onClickButton={() => console.log("hello")}>
-        Click here
-      </Button>
+      {display && (
+        <Alert onClose={() => handleOnClick(false)}>
+          Hello <b>World!</b>
+        </Alert>
+      )}
+      <Button onClickButton={() => handleOnClick(true)}>My Button</Button>
     </div>
   );
 }
